c4535b7a5c7f770707e0267944bd1a4647869fa0
Richard Hein <pyaesone.dev.web@gmail.com>
2025-12-18 20:03:51 +0630

    add wave image

--- src/sections/MyJourney.jsx
+++ src/sections/MyJourney.jsx
@@ -3,7 +3,7 @@ import {motion} from "motion/react"
 const MyJourney = () => {
   return (
     <>
-    <img src="./public/images/bluewave.jpg" className="wave-img h-100  " />
+    <img src="https://clhwgpfvszfxjhqtvypy.supabase.co/storage/v1/object/sign/image/bluewave.jpg?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV9lZjRkNDkwNi1hM2E0LTQzMGMtODFlNC0yN2M0ZDlmYTBhMWUiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJpbWFnZS9ibHVld2F2ZS5qcGciLCJpYXQiOjE3NjYwNjQ3MzYsImV4cCI6MTc5NzYwMDczNn0.tXV8LpluCD8HaB7tad9rP5Arqfn5-amXTuxNoos2QwQ" className="wave-img h-100 inline-block " />
     <section className="grid grid-cols-1 gap-0 md:grid-cols-2  sm:gap-2 max-w-[1000px] mx-auto mt-[80px]">
        <div className="mx-4">
         <h2 className="text-[1.5rem] md:text-[1.2rem] font-bold text-center underline underline-offset-6 decoration-2 decoration-primary">My Education</h2>
